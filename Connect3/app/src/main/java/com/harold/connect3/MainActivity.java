5b5cdf48ecfe49346b92b83db04001f65d71ef91
haroldyeo <haroldyeo@gmail.com>
2019-06-10 00:08:11 -0500

    connect3 almost done

--- Connect3/app/src/main/java/com/harold/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/harold/connect3/MainActivity.java
@@ -2,24 +2,93 @@ package com.harold.connect3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO: IN CASE OF DRAW
+
     // 1 => red, 2=> yellow, 0 => empty
-    String activePlayer = "1";
+    int activePlayer = 1;
+    int[] gameState = {0,0,0,0,0,0,0,0,0,0};
+    int[][] winningPositions = {{1,2,3}, {4,5,6}, {7,8,9}, {1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7}};
+    boolean gameOver = false;
+    int winner = 0;
 
     public void diveIn(View view){
 
-        ImageView piece = (ImageView)view;
+        if(!gameOver){
 
-        // piece movement
-        piece.setImageResource(R.drawable.red);
-        piece.setTranslationY(-1500);
-        piece.animate().rotation(2400).translationYBy(1500);
+            ImageView piece = (ImageView)view;
+
+            // set game state
+            if(gameState[Integer.parseInt((String)piece.getTag())]==0){
+                gameState[Integer.parseInt((String)piece.getTag())] = activePlayer;
+
+                // set active player
+                if(activePlayer==1){
+                    piece.setImageResource(R.drawable.red);
+                    activePlayer = 2;
+                } else{
+                    piece.setImageResource(R.drawable.yellow);
+                    activePlayer = 1;
+                }
+
+                // piece movement
+                piece.setTranslationY(-1500);
+                piece.animate().rotation(3600).translationYBy(1500);
+
+
+                // check winning position
+                for(int[] wp : winningPositions){
+                    if(gameState[wp[0]] == gameState[wp[1]] && gameState[wp[1]]==gameState[wp[2]] && gameState[wp[0]]!=0){
+                        //Toast.makeText(this,gameState[wp[0]]==1 ? " RED WINS " : "YELLOW WINS", Toast.LENGTH_LONG).show();
+                        winner = gameState[wp[0]];
+
+                        // ask for play again
+                        Button btnPlayAgain = (Button) findViewById(R.id.btnPlayAgain);
+                        TextView txtWinner = (TextView) findViewById(R.id.txtWinner);
+                        txtWinner.setText(winner==1 ? "RED WINS" : "YELLOW WINS");
+                        btnPlayAgain.animate().alpha(1);
+                        txtWinner.animate().alpha(1);
+                        gameOver = true;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    public void playAgain(View view){
+        Button btnPlayAgain = (Button) findViewById(R.id.btnPlayAgain);
+        TextView txtWinner = (TextView) findViewById(R.id.txtWinner);
+        for(int i=0; i<gameState.length;i++){
+            gameState[i] = 0;
+        }
+        gameOver = false;
+        winner = 0;
+
+        btnPlayAgain.animate().alpha(0);
+        txtWinner.animate().alpha(0);
+        resetBoard();
+    }
+
+    private void resetBoard() {
+
+        GridLayout grid = findViewById(R.id.gridLayout);
+        for(int i=0; i<grid.getChildCount();i++){
+            if(grid.getChildAt(i) instanceof ImageView){
+                ((ImageView) grid.getChildAt(i)).setImageResource(0);
+            }
+        }
 
     }
 
