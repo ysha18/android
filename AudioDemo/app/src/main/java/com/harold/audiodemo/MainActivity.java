860e5bbb9851807fdd471ee7c73417bfee8b4d48
haroldyeo <haroldyeo@gmail.com>
2019-07-04 20:05:49 -0500

    audio project done

---
+++ AudioDemo/app/src/main/java/com/harold/audiodemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.harold.audiodemo;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.marbles);
+    }
+
+    public void play(View view){
+        mediaPlayer.start();
+
+    }
+
+    public void pause(View view){
+            mediaPlayer.pause();
+    }
+}
