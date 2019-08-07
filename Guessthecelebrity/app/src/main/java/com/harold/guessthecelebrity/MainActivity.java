c05cd5637eae3fb44b34c94b43c095cc1a1074ca
haroldyeo <haroldyeo@gmail.com>
2019-08-06 23:23:48 -0500

    Guess the celebrity on going

--- Guessthecelebrity/app/src/main/java/com/harold/guessthecelebrity/MainActivity.java
+++ Guessthecelebrity/app/src/main/java/com/harold/guessthecelebrity/MainActivity.java
@@ -1,15 +1,18 @@
-package com.example.downloadingwebcontents;
+package com.harold.guessthecelebrity;
 
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... urls) {
-//            Log.i("URL",strings[0]);
 
-            String result = "";
+            String result = null;
             URL url;
             HttpURLConnection urlConnection = null;
 
@@ -31,37 +33,51 @@ public class MainActivity extends AppCompatActivity {
                 int data = reader.read();
 
                 while (data!=-1){
-                    char current = (char)data;
+                    char current =(char)data;
                     result += current;
                     data = reader.read();
                 }
 
-                return result;
-
-
-            } catch (Exception e) {
+            } catch (MalformedURLException e) {
                 e.printStackTrace();
-                return "Failed";
+            } catch (IOException e) {
+
+
             }
-
-
+            return result;
         }
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String result = null;
-
-        DownloadTask task =  new DownloadTask();
-        try{
-            result = task.execute("http://www.zappycode.com").get();
-        } catch (Exception e){
-            Log.i("Exception: ", e.getMessage());
+        try {
+            generateImage();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
+    }
+
+    private void generateImage() throws ExecutionException, InterruptedException {
+
+        // get HTMl of all images and names
+        String result = null;
+        DownloadTask task = new DownloadTask();
+        result = task.execute("http://www.posh24.se/kandisar").get();
         Log.i("RESULT",result);
+
+        // Using String manipulation, create 2 maps: id->name and id->url
+
+        // randomly get id to display, get name and url
+
+        // randomly get 3 ids with names names
+
+        // display all
     }
 }
