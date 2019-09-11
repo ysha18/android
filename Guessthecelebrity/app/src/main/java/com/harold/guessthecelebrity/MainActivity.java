6f4caf2cd99a15889fcdf6a03c8353719a13ddc7
Yeo, Harold <harold.yeo@bbva.com>
2019-09-10 23:21:12 -0500

    Guess the celeb: Celeb selected and displayed

--- Guessthecelebrity/app/src/main/java/com/harold/guessthecelebrity/MainActivity.java
+++ Guessthecelebrity/app/src/main/java/com/harold/guessthecelebrity/MainActivity.java
@@ -2,10 +2,10 @@ package com.harold.guessthecelebrity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.IOException;
@@ -16,8 +16,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,6 +27,8 @@ import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    Bitmap myBitmap;
+    Button btn1,btn2,btn3,btn4;
 
     public class DownloadTask extends AsyncTask<String,Void,String>{
 
@@ -51,8 +55,9 @@ public class MainActivity extends AppCompatActivity {
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
-
-
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
             return result;
         }
@@ -70,9 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 connection.connect();
                 InputStream in = connection.getInputStream();
 
-                Bitmap myBitmap = BitmapFactory.decodeStream(in);
-
-                imageView.setImageBitmap(myBitmap);
+                myBitmap = BitmapFactory.decodeStream(in);
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -91,6 +94,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
 
         try {
             generateImage();
@@ -117,11 +124,12 @@ public class MainActivity extends AppCompatActivity {
 
         Pattern p = Pattern.compile("<div class=\"image\">"+"(?s)"+"(.*?)"+"</div>");
         Matcher m = p.matcher(result);
-        List<String> celebs = new ArrayList<>();
+        List<String> celebsNameAndLink = new ArrayList<>();
+        List<String> celebsName = new ArrayList<>();
 
         while(m.find()){
 //            System.out.println(m.group(1));
-            celebs.add(m.group(1));
+            celebsNameAndLink.add(m.group(1));
         }
 
 
@@ -129,12 +137,13 @@ public class MainActivity extends AppCompatActivity {
         Pattern pName = Pattern.compile("alt=\""+"(?s)"+"(.*?)"+"\"");
         Matcher mLink,mName;
         String name = null;
-        for(int i = 0; i < celebs.size(); i++){
-            mName = pName.matcher((celebs.get(i)));
+        for(int i = 0; i < celebsNameAndLink.size(); i++){
+            mName = pName.matcher((celebsNameAndLink.get(i)));
             while(mName.find()){
                 name = mName.group(1);
+                celebsName.add(name);
             }
-            mLink = pLink.matcher(celebs.get(i));
+            mLink = pLink.matcher(celebsNameAndLink.get(i));
             while(mLink.find()){
                 mapCelebs.put(name,mLink.group(1));
             }
@@ -144,11 +153,37 @@ public class MainActivity extends AppCompatActivity {
         /*
          * randomly get id to display, get name and url
          */
-            DownloadImageTask imageTask = new DownloadImageTask();
-            imageTask.execute(mapCelebs.get("Kim Kardashian"));
+        Random rand = new Random();
+        String selectedCeleb = celebsName.get(rand.nextInt(celebsNameAndLink.size()+1));
 
+        DownloadImageTask imageTask = new DownloadImageTask();
+        imageTask.execute(mapCelebs.get(selectedCeleb));
+        Thread.sleep(3000);
 
+        int celebPosition = 0;
+
+        do{
+            celebPosition = rand.nextInt(5);
+        } while (celebPosition==0);
+
+        switch (celebPosition){
+            case 1 : btn1.setText(selectedCeleb);
+            break;
+            case 2 : btn2.setText(selectedCeleb);
+                break;
+            case 3 : btn3.setText(selectedCeleb);
+                break;
+            case 4 : btn4.setText(selectedCeleb);
+                break;
+        }
+
+        imageView.setImageBitmap(myBitmap);
+
+        /*
         // randomly get 3 ids with names names
+        */
+//        String selectedCeleb = celebsName.get(rand.nextInt(celebsNameAndLink.size()+1));
+
 
 
         // display all
